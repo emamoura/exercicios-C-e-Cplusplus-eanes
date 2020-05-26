434d6f95708d1e19dbd095fe98a144671e5c90d3
emamoura <emanuel.moura@ccc.ufcg.edu.br>
2020-05-26 12:15:27 -0300

    Solução "Mês do ano"

---
+++ Exercícios 2/meses.c
@@ -0,0 +1,33 @@
+        break;
+    case 4:
+        printf("Abril - 30 dias\n");
+        break;
+    case 5:
+        printf("Maio - 31 dias\n");
+        break;
+    case 6:
+        printf("Junho - 30 dias\n");
+        break;
+    case 7:
+        printf("Julho - 31 dias\n");
+        break;
+    case 8:
+        printf("Agosto - 31 dias\n");
+        break;
+    case 9:
+        printf("Setembro - 30 dias\n");
+        break;
+    case 10:
+        printf("Outubro - 31 dias\n");
+        break;
+    case 11:
+        printf("Novembro - 30 dias\n");
+        break;
+    case 12:
+        printf("Dezembro - 31 dias\n");
+        break;        
+    default:
+        printf("O valor digitado não corresponde a nenhum mês\n");
+        break;
+    }
+}
